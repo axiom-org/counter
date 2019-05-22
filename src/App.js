3807118305df8ca0977e459a6568b6cd087b249d
Kevin Lacker <lacker@gmail.com>
2019-05-22 16:55:30 -0700

    better counter logic

--- src/App.js
+++ src/App.js
@@ -9,26 +9,84 @@ export default class App extends React.Component {
 
     this.state = {
       publicKey: null,
+      loadingMessage: null,
       bucket: null,
-      counter: 0
+      counter: null
     };
 
     this.login = this.login.bind(this);
+    this.createBucket = this.createBucket.bind(this);
+    this.increment = this.increment.bind(this);
   }
 
   async login() {
     let publicKey = await this.axiom.getPublicKey();
-    this.setState({ publicKey: publicKey });
+    this.setState({
+      publicKey: publicKey
+    });
+
+    this.setState({
+      loadingMessage: "loading your data..."
+    });
+
+    let bucket = await this.axiom.getBucket("counter", this.state.publicKey);
+    if (bucket) {
+      console.log("XXX got bucket:", bucket);
+      let data = await bucket.getJSON("counter.json");
+      console.log("XXX got data:", data);
+      let counter = data ? data.counter : 0;
+      this.setState({
+        bucket: bucket,
+        counter: counter,
+        loadingMessage: null
+      });
+    } else {
+      console.log("XXX failed to get bucket");
+      this.setState({
+        loadingMessage: null
+      });
+    }
+  }
+
+  async createBucket() {
+    this.setState({
+      loadingMessage: "creating a bucket..."
+    });
+    console.log("XXX creating a bucket");
+    await this.axiom.createBucket("counter", this.state.publicKey, 1);
+    await this.login();
+  }
+
+  async increment() {
+    this.setState({
+      loadingMessage: `counting to ${this.state.counter + 1}...`
+    });
+    this.state.bucket.setJSON("counter.json", {
+      counter: this.state.counter + 1
+    });
+    console.log("XXX uploading...");
+    await this.state.bucket.upload();
+    console.log("XXX upload complete");
+    this.setState({ waiting: false, counter: this.state.counter + 1 });
   }
 
   render() {
+    if (this.state.loadingMessage) {
+      return <p>{this.state.loadingMessage}</p>;
+    }
+
     if (!this.state.publicKey) {
       return <button onClick={this.login}>Log In</button>;
     }
 
+    if (!this.state.bucket) {
+      return <button onClick={this.createBucket}>Create a Counter</button>;
+    }
+
     return (
       <div>
-        <p>Hello {this.state.publicKey}</p>
+        <p>Counter: {this.state.counter}</p>
+        <button onClick={this.increment}>count!</button>
       </div>
     );
   }
