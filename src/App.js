eccc99891649cce126d3b8261791c3fc821b1a30
Kevin Lacker <lacker@gmail.com>
2019-05-22 13:38:45 -0700

    debug this

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 import Axiom from "axiom-api";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.axiom = new Axiom();
+    this.axiom = new Axiom({ verbose: true });
 
     this.state = {
+      publicKey: null,
       bucket: null,
       counter: 0
     };
+
+    this.login = this.login.bind(this);
+  }
+
+  async login() {
+    let publicKey = await this.axiom.getPublicKey();
+    this.setState({ publicKey: publicKey });
   }
 
   render() {
+    if (!this.state.publicKey) {
+      return <button onClick={this.login}>Log In</button>;
+    }
+
     return (
       <div>
-        <p>Hello decentralized world</p>
+        <p>Hello {this.state.publicKey}</p>
       </div>
     );
   }
